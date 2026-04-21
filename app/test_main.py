2f35cc1e3f5db424974eafde118d50d1a68ffc4c
David <meeddry@gmail.com>
2026-04-22 01:14:05 +0300

    solution_2

--- app/test_main.py
+++ app/test_main.py
@@ -2,13 +2,13 @@ from unittest.mock import patch, Mock
 from app.main import cryptocurrency_action
 
 
-@patch("app.main.get_exchange_rate_prediction", return_value=105)
+@patch("app.main.get_exchange_rate_prediction", return_value=106)
 def test_buy(mock_prediction: Mock) -> None:
     result = cryptocurrency_action(100)
     assert result == "Buy more cryptocurrency"
 
 
-@patch("app.main.get_exchange_rate_prediction", return_value=95)
+@patch("app.main.get_exchange_rate_prediction", return_value=94)
 def test_sell(mock_prediction: Mock) -> None:
     result = cryptocurrency_action(100)
     assert result == "Sell all your cryptocurrency"
